3add670e1ad339a0081d1c94470166b9fa6ef056
jonathan <jaadams12@outlook.com>
2025-04-28 00:11:12 +0100

    feat: implement database connection and update server configuration; add dotenv for environment variables

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import path, { dirname } from 'path';
 import bodyParser from 'body-parser';
 import roots from './Roots/taskRoots.js';
 import dotenv from 'dotenv';
+import { connectDB} from './config/db.js';
 import mongoose from 'mongoose';
 import { access } from 'fs';
 
@@ -11,11 +12,8 @@ import { access } from 'fs';
 const app = express();
 const __dirname = path.resolve();
 dotenv.config();
-const PORT = process.env.PORT || 8000;
-const MONGODB_URI = process.env.DATABASE_URL || "mongodb+srv://Jon:ROAG9EbbqUP3xpm5@testforcloudform.beyvtfq.mongodb.net/";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+const PORT = process.env.PORT || 5000;
+
 
 const corsOptions = {
 origin: `*`, // Allow all origins (not recommended for production)
@@ -23,7 +21,7 @@ origin: `*`, // Allow all origins (not recommended for production)
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -33,10 +31,31 @@ app.use((req, res, next) => {
   next();
 });
 app.use('/', roots);
-
-
+app.get('/', (req, res) => {
+  res.status(200).send('Server is running');
+});
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
+  
+    connectDB()
+        .then(() => {
+            console.log('MongoDB connected successfully');
+        })
+        .catch((error) => {
+            console.error('MongoDB connection error:', error);
+        });
     console.log(`Server is running on port ${PORT}`);
     });
-
+process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+        console.log('MongoDB connection closed');
+        process.exit(0);
+    });
+}
+);
